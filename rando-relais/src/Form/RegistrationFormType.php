cc914daf9b78734a1ea8f15db724c34b97ddd8f6
Aimerick FERAL <feralaimerick@gmail.com>
2021-07-21 11:57:05 +0200

    delete of the label password in registration form

--- rando-relais/src/Form/RegistrationFormType.php
+++ rando-relais/src/Form/RegistrationFormType.php
@@ -79,7 +79,6 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                // TODO : delete de password label.
                 'label'  => false,
                 'attr'   => [
                     'autocomplete'  => 'new-password',
