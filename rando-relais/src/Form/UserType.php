432a22ae0b0cbe8cb92b8ea02bece6b88ec84531
timothy974 <timothy.gomis@gmail.com>
2021-07-21 17:50:52 +0200

    edit user form (backoffice)

--- rando-relais/src/Form/UserType.php
+++ rando-relais/src/Form/UserType.php
@@ -2,9 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Service;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -15,19 +21,45 @@ class UserType extends AbstractType
             ->add('firstName')
             ->add('lastName')
             ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Utilisateur' => 'ROLE_USER',
+                ],
+                'multiple' => true,
+                'expanded' => true
+            ])
             ->add('city')
             ->add('zipCode')
             ->add('picture')
             ->add('phoneNumber')
-            ->add('latitude')
-            ->add('longitude')
             ->add('status')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('services')
-        ;
+            ->add('services', EntityType::class, [
+                'by_reference' => false,
+                'class' => Service::class,
+                'required'=>false,
+                'multiple' => true,
+                'attr' => ['size' => 10]
+            ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+
+                $form = $event->getForm();
+                $userData = $event->getData();
+
+                if ($userData->getId() === null) {
+                    // if we are in a creation mode password will be required
+                    $required = true;
+                    $form->add('password', PasswordType::class, [
+                        'mapped' => false,
+                        'required' => $required
+                    ]);
+                } else {
+                    // if we are in an update mode don't need password
+                    $required = false;
+                }
+
+                
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver)
