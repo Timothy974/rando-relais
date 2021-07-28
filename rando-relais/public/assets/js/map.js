a96d5e61916bfc3155cab4756577015cf2afab0e
timothy974 <timothy.gomis@gmail.com>
2021-07-28 17:26:32 +0200

    get the lat lon data from db to target a circle on the map

--- rando-relais/public/assets/js/map.js
+++ rando-relais/public/assets/js/map.js
@@ -1,15 +1,19 @@
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+let lat = document.querySelector('#mapid').dataset.lat;
+let lon = document.querySelector('#mapid').dataset.lon;
+
+let mymap = L.map('mapid').setView([lat, lon], 13);
 
 L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-
+    maxZoom: 18,   
 }).addTo(mymap);
 
-let circle = L.circle([51.508, -0.11], {
-   color: 'red',
-   fillColor: '#f03',
+// Add a circle to target the location
+let circle = L.circle([lat, lon], {
+   color: 'green',
+   fillColor: '#198754',
    fillOpacity: 0.5,
    radius: 500
 }).addTo(mymap);
