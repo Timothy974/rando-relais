9d31464582a3dd240b25b73f3e11cd759f571e20
timothy974 <timothy.gomis@gmail.com>
2021-07-29 14:20:28 +0200

    add a slug in addressApi service to slug city name for api request

--- rando-relais/src/Service/AddressApi.php
+++ rando-relais/src/Service/AddressApi.php
@@ -2,16 +2,19 @@
 
 namespace App\Service;
 
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AddressApi 
 {
    private $client;
+   private $slugger;
    private $ApiUrl = 'https://api-adresse.data.gouv.fr/search/?q=';
 
-   public function __construct(HttpClientInterface $client)
+   public function __construct(HttpClientInterface $client, SluggerInterface $slugger)
    {
-      return $this->client = $client;
+      $this->client = $client;
+      $this->slugger = $slugger;
    }
 
    /**
@@ -23,8 +26,10 @@ class AddressApi
     */
    public function getCoordinatesWithAddress ($city, $zipCode)
    {
+      // We slug the city name
+      $citySlug = $this->slugger->slug($city);
       // client do the request
-      $response = $this->client->request('GET', $this->ApiUrl.$city.'&postcode='.$zipCode);
+      $response = $this->client->request('GET', $this->ApiUrl.$citySlug.'&postcode='.$zipCode);
 
       return $response->toArray();
    }
